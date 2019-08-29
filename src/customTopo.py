0504a5bfcaf80b07c811c6264946438e62a119a7
guerraUtfpr <guerramatheus2@gmail.com>
2019-08-29 10:20:36 -0300

    testando pktCreate

--- src/customTopo.py
+++ src/customTopo.py
@@ -242,7 +242,7 @@ def tests(net):
 			hostDestLabel.cmd("echo python pktCreate.py --es --" + test.protocol + " " + test.destinationIP + " --sport " + test.sourcePort)
 			#start client
 			info("*** conectando cliente *** \n")
-			info("python pktCreate.py --ec --" + test.protocol + " " + test.sourceIP + " --dport " + test.sourcePort)
+			hostSourceLabel("python pktCreate.py --ec --" + test.protocol + " " + test.sourceIP + " --dport " + test.destinationPort)
 			info('*** Testing...***\n')
 			time.sleep(10)
 			pass
@@ -259,9 +259,9 @@ def tests(net):
 		for iface in host.iface:
 			command = Command(iface)
 			hostNET = net.getNodeByName(host.label)
-			#hostNET.cmd(command.convertLogTcpdump())
+			hostNET.cmd(command.convertLogTcpdump())
 			#time.sleep(3)
-			#hostNET.cmd("rm " + iface.name + ".log")
+			hostNET.cmd("rm " + iface.name + ".log")
 
 
 
