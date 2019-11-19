667dad57b2e95a88e28255fd962ba8855c897ca7
guerraUtfpr <guerramatheus2@gmail.com>
2019-11-19 18:01:29 -0300

    testando 2 redes

---
+++ src/aaa.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from mininet.net import Mininet
+from mininet.node import Controller
+from mininet.cli import CLI
+from mininet.log import setLogLevel, info
+from mininet.node import OVSSwitch, Controller, RemoteController
+import time
+import os
+import json
+import time
+import timeit
+
+def emptyNet():
+	net = Mininet(controller=Controller)
+
+	info('*** Adding hosts ***\n')
+        net.addHost('h1')
+        net.addHost('h2')
+
+        net.addSwitch('s1')
+
+        net.addHost('r1')
+
+        net.addSwitch('s2')
+
+        net.addHost('h3')
+        net.addHost('h4')
+
+
+	info('*** Creating links ***\n')
+        net.AddLink('h1','s1')
+        net.AddLink('h2','s1')
+
+        net.AddLink('s1', 'r1')        
+        net.AddLink('r1', 's2')
+
+        net.AddLink('h3','s2')
+        net.AddLink('h4','s2')
+
+
+	info('*** Configuring hosts ***\n')
+
+
+	info('*** Init tests ***\n')
+	tests(net)
+
+	info('*** Starting CLI ***\n')
+	CLI(net)
+
+	info('*** Stopping newtwork ***\n')
+	net.stop()
+	exit()
+
+
+
+
+setLogLevel('info')
+emptyNet()
+
+
+## Não estamos pensando em executar serviços de rede, tal como ftp
