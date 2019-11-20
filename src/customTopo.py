be2223e59a564101c2039e89e50e45c21436253e
guerraUtfpr <guerramatheus2@gmail.com>
2019-11-20 18:35:15 -0300

    teste 2 redes

--- src/customTopo.py
+++ src/customTopo.py
@@ -384,7 +384,6 @@ def result(test):
 	
 	for line in lines:
 		if("Flags" in line):
-			info(str(handshake) + " > dentro do laço\n")
 			processedLine = processTcpdumpLine(line)
 			if(aux ==  0 and test.sourceIP == processedLine[1] and test.destinationIP == processedLine[2]):
 				if(processedLine[3] == "[S]"):				
@@ -410,7 +409,7 @@ def result(test):
 			if(test.sourceIP == processedLine[1]):
 				datagram = True
 				break
-	info(str(handshake) + " > fora do laço\n")
+
 	if(test.protocol == "tcp"):
 		if(test.expected == "accept"):
 			if(handshake == True):
@@ -419,7 +418,7 @@ def result(test):
 				info("\nTeste REPROVADO - os pacotes não chegaram ao destino")
 
 		if(test.expected == "deny"):
-			if(handshake == True):
+			if(handshake != True):
 				info(str(test.expected) + ' ' + str(handshake) + "\n")
 				info("\nTeste REPROVADO - os pacotes chegaram ao destino")			
 			else:
@@ -436,7 +435,7 @@ def result(test):
 				info("\nTeste REPROVADO - os pacotes não chegaram ao destino")
 
 		if(test.expected == "deny"):
-			if(datagram == True):
+			if(datagram != True):
 				info("\nTeste REPROVADO - os pacotes chegaram ao destino")			
 			else:
 				info("\nTeste APROVADO - os pacotes não chegaram ao destino")
